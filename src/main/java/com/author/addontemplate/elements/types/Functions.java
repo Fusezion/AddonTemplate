b4eef7cf937d7b7cf92250c7b74019dbb59f16fc
Fusezion <fusezionstream@gmail.com>
2023-07-30 02:52:00 -0400

    elements/types/functions - Updates Added doc annotations Added register method generics

--- src/main/java/com/author/addontemplate/elements/types/Functions.java
+++ src/main/java/com/author/addontemplate/elements/types/Functions.java
@@ -1,36 +1,28 @@
 package com.author.addontemplate.elements.types;
 
-import ch.njol.skript.classes.ClassInfo;
-import ch.njol.skript.classes.data.BukkitClasses;
-import ch.njol.skript.classes.data.DefaultFunctions;
-import ch.njol.skript.lang.function.FunctionEvent;
 import ch.njol.skript.lang.function.JavaFunction;
 import ch.njol.skript.lang.function.Parameter;
 import ch.njol.skript.lang.function.SimpleJavaFunction;
-import ch.njol.skript.lang.util.SimpleLiteral;
 import ch.njol.skript.registrations.Classes;
 import ch.njol.skript.registrations.DefaultClasses;
 import ch.njol.util.coll.CollectionUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.eclipse.jdt.annotation.Nullable;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
-import java.util.regex.Pattern;
 
 public class Functions {
 
     static {
 
-        Parameter<?>[] stringParam = new Parameter[]{new Parameter<>("string", DefaultClasses.STRING, true, null)};
+        register(new SimpleJavaFunction<OfflinePlayer>("player", new Parameter[]{
+                new Parameter<>("nameOrUUID", DefaultClasses.STRING, true, null)
 
-        register(new SimpleJavaFunction<>("player", new Parameter[]{
-                new Parameter("nameOrUUID", DefaultClasses.STRING, true, null)
-
-        }, Classes.getExactClassInfo(OfflinePlayer.class),true) {
+        }, Classes.getExactClassInfo(OfflinePlayer.class), true) {
             @Override
-            public @Nullable OfflinePlayer[] executeSimple(Object[][] params) {
+            @Nullable
+            public OfflinePlayer[] executeSimple(Object[][] params) {
                 String nameOrUUID = (String) params[0][0];
                 UUID uuid = null;
                 try {
@@ -39,13 +31,16 @@ public class Functions {
                 return CollectionUtils.array(uuid != null ? Bukkit.getOfflinePlayer(uuid) : Bukkit.getOfflinePlayer(nameOrUUID));
             }
         }
-                .description("")
-                .examples("")
-                .since(""));
+                .description("Retrieve an offline player object without needing to use ExprParse")
+                .examples(
+                        "on load:",
+                        "\tset {_playerFromName} to player(\"Fusezion\")",
+                        "\tset {_playerFromUUID} to player(uuid of {_playerFromName})"
+                ).since("INSERT VERSION"));
     }
 
     // Shortcut method as skript used Functions too
-    private static void register(JavaFunction<?> function) {
+    private static <T> void register(JavaFunction<T> function) {
         ch.njol.skript.lang.function.Functions.registerFunction(function);
     }
 
